e53495075bff85db70681cfde183c587a9378d20
kinokino <flowers19961218@gmail.com>
2019-05-23 12:48:28 +0900

    no message

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,23 @@
+//カードの雛形
+
+let cardTemplate = null;
+
+
 $(function(){
+
+    // card.htmlを読み込む
+    $.get('../../card.html',function(temp) {
+     cardTemplate = $(temp);
+    })
+
+
     $('#search-btn').on('click',function(){
   //検索ボタンがクリックされたら
+
+
+
+//検索ワードを取得する
+let searchword = $('#search-word').val();
 
         //itunesに検索をしに行く(Ajax)
         $.ajax({
@@ -8,13 +25,41 @@ $(function(){
             type:'GET', //ＧＥＴ送信 or ＰＯＳＴ送信
             dataType:'jsonp', //検索結果の形式
             data: {
-                term:'あいみょん',
+                term: searchword,
                 country: 'jp',
             }
 
         }).done( (data) => {
             //通信が成功
-            console.log(data);
+            $('#result').empty();
+            //CDの画像
+
+            for (item of data.results) {
+                // 画像のパス
+                let imgPath = item.artworkUrl100;
+        
+                // CDタイトル
+                let collectionName = item.collectionName;
+        
+                // iTunesのリンク
+                let collectionViewUrl = item.collectionViewUrl;
+                
+                //  変数にテンプレートのクローンを入れる
+                let card = cardTemplate.clone();
+
+                //クローンにタイトルなどを設定
+                card.find('img').attr('src',imgPath);
+                card.find('h5').text(collectionName);
+                card.find('a').attr('href',collectionViewUrl);
+               
+               //クローンを#resultに追加する
+                $('#result').append(card);
+                
+              }
+
+              
+            
+
         }).fail((data) => {
             //通信が失敗
             console.log('error');
@@ -22,3 +67,5 @@ $(function(){
     })
 
 });
+
+
